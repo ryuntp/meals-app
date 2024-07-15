5336b484be676b8c82934278f5e283dfa8bea8c9
ryuntp <ryu_r@hotmail.com>
2024-07-15 16:35:57 +0700

    implement bloc to tabs

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:meals/models/meal.dart';
-import 'package:meals/providers/favorites_provider.dart';
+import 'package:meals/bloc/favorites/favorites_cubit.dart';
+import 'package:meals/bloc/favorites/favorites_state.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
-// import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/providers/filters_provider.dart';
 
 const kInitialFilters = {
@@ -67,7 +67,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
       await Navigator.of(context).push<Map<Filter, bool>>(
-        // final result = await Navigator.of(context).push<Map<Filter, bool>>(
+         // final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
           builder: (ctx) {
             return const FiltersScreen();
@@ -90,11 +90,15 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     var activePageTitle = 'Categories';
 
     if (_selectedPageIndex == 1) {
-      final favoriteMeals = ref.watch(favoriteMealsProvider);
-      activePage = MealsScreen(
-        meals: favoriteMeals,
+      activePage = BlocBuilder<FavoritesCubit, FavoritesState>(
+        builder: (context, state) {
+          final favoriteMeals = state.favoriteMeals;
+          return MealsScreen(
+            meals: favoriteMeals,
+          );
+        },
       );
-      activePageTitle = 'Favourites';
+      activePageTitle = 'Favorites';
     }
 
     return Scaffold(
